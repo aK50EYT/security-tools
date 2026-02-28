f3372e873248904d895acc7714f2a03b262f3a6a
aK50EYT <anaskennouz7@gmail.com>
2026-02-28 05:31:25 +0000

    Add service detection  feature

--- port-scanner/scanner.py
+++ port-scanner/scanner.py
@@ -1,5 +1,19 @@
 import socket
 
+# Dictionnaire des services
+SERVICES = {
+    21: "FTP",
+    22: "SSH",
+    23: "Telnet",
+    80: "HTTP",
+    443: "HTTPS",
+    8080: "HTTP-ALT",
+    3306: "MySQL",
+    3389: "RDP",
+    6379: "Redis",
+    27017: "MongoDB"
+}
+
 def resolve_domain(target):
     try:
         ip = socket.gethostbyname(target)
@@ -9,12 +23,16 @@ def resolve_domain(target):
         print(f"[-] Could not resolve {target}")
         return None
 
+def get_service(port):
+    return SERVICES.get(port, "Unknown")
+
 def scan_port(target, port):
     try:
         s = socket.socket()
         s.settimeout(1)
         s.connect((target, port))
-        print(f"[+] Port {port} OPEN")
+        service = get_service(port)
+        print(f"[+] Port {port} OPEN  →  {service}")
         s.close()
     except:
         print(f"[-] Port {port} CLOSED")
@@ -27,5 +45,4 @@ def scan_multiple_ports(target, ports):
             scan_port(ip, port)
 
 target = input("Enter target IP or domain: ")
-scan_multiple_ports(target, [21, 22, 23, 80, 443, 8080])
-
+scan_multiple_ports(target, [21, 22, 23, 80, 443, 8080, 3306, 3389])
